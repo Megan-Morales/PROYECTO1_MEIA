717f8d9e050e71eded6680d5cee213649f82d5e2
Megan Morales <63917946+Megan-Morales@users.noreply.github.com>
2022-10-04 13:14:46 -0600

    Commit inicial

---
+++ src/clases/Usuario.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+import java.util.Date;
+
+
+/**
+ *
+ * @author megan
+ */
+public class Usuario {
+    
+    String Usuario;
+    String Nombre;
+    String Apellido;
+    String Password;
+    byte Rol;
+    Date fecha_nacimiento;
+    String correo_alterno;
+    int Telefono;
+    String Path_fotografia;
+    byte Estatus;
+}
---
+++ src/proyecto1_meia/Proyecto1_MEIA.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1_meia;
+
+/**
+ *
+ * @author megan
+ */
+public class Proyecto1_MEIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
